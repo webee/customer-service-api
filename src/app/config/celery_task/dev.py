66530a456482a649dad2ee6552a4c0623cc687c2
wang.yi <wang.yi@qinqinxiaobao.com>
2017-11-09 16:17:20 +0800

    update configs.

--- src/app/config/celery_task/dev.py
+++ src/app/config/celery_task/dev.py
@@ -1,5 +1,10 @@
 from datetime import timedelta
 
+broker_pool_limit = 4
+
+worker_prefetch_multiplier = 4
+worker_max_tasks_per_child = 1000
+
 
 beat_schedule = {
     'test_job-E30m': {
--- src/app/config/celery_task/test.py
+++ src/app/config/celery_task/test.py
@@ -1,1 +1,19 @@
+from datetime import timedelta
 
+broker_url = 'redis://127.0.0.1:6379/8'
+
+broker_pool_limit = 4
+
+worker_prefetch_multiplier = 4
+worker_max_tasks_per_child = 1000
+
+
+beat_schedule = {
+    'test_job-E1d': {
+        'task': 'app.task.tasks.test_job',
+        'schedule': timedelta(days=1),
+        'kwargs': {},
+        'relative': True,
+        'options': {'queue': 'celery_periodic'}
+    },
+}
