aae6db95a2014e4f68e11ac1d568c17a9d528aba
wang.yi <wang.yi@qinqinxiaobao.com>
2017-11-10 13:50:44 +0800

    update customer apis.

--- src/app/apis/namespaces/customer/__init__.py
+++ src/app/apis/namespaces/customer/__init__.py
@@ -2,3 +2,4 @@
 面向客户端的接口
 """
 from . import resources
+from . import auth_resources
---
+++ src/app/apis/namespaces/customer/auth_resources.py
@@ -0,0 +1,24 @@
+from flask_restplus import Resource
+from app import jwt
+from app.apis.jwt import current_customer, require_customer
+from .api import api
+from app.apis.serializers.auth import token_data
+from app.apis.serializers.customer import customer
+
+
+@api.route('/auth')
+class Auth(Resource):
+    @require_customer
+    @api.marshal_with(customer)
+    def get(self):
+        """得到customer信息"""
+        return current_customer
+
+
+@api.route('/auth/refresh_token')
+class RefreshToken(Resource):
+    @require_customer
+    @api.marshal_with(token_data)
+    def post(self):
+        """刷新customer token"""
+        return dict(token=jwt.encode_token('customer', current_customer))
