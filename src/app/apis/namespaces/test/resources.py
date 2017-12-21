69bbeb8b14d80334a716b4297980777adfdd947d
wang.yi <wang.yi@qinqinxiaobao.com>
2017-12-22 04:16:09 +0800

    add some test.

--- src/app/apis/namespaces/test/resources.py
+++ src/app/apis/namespaces/test/resources.py
@@ -1,4 +1,9 @@
+import threading
+import multiprocessing
+from multiprocessing import Value
+import time
 import json
+from datetime import datetime, timedelta
 from flask_restplus import Resource, abort
 from app import errors
 from app.errors import BizError
@@ -21,3 +26,41 @@ class TestAbort(Resource):
 class TestBizError(Resource):
     def get(self):
         raise BizError(errors.ERR_XXX, 'what ever xxx', dict(ns='test', path='/biz_error'))
+
+
+tlock = threading.RLock(blocking=False)
+
+
+@api.route('/test_threads')
+class TestThreads(Resource):
+    def get(self):
+        ret = False
+        with tlock as lock:
+            if lock:
+                time.sleep(2)
+                ret = True
+        return dict(ret=ret, tident=threading.get_ident(), pident=multiprocessing.current_process().ident)
+
+
+plock = multiprocessing.RLock()
+pn = [0]
+pln = Value('i', 0)
+
+
+@api.route('/test_processes')
+class TestProcesses(Resource):
+    def get(self):
+        n = datetime.utcnow()
+        ret = False
+        lock = plock.acquire(block=False)
+        if lock:
+            while True:
+                if datetime.utcnow() - n > timedelta(seconds=2):
+                    break
+            # time.sleep(1)
+            ret = True
+            plock.release()
+        pln.value += 1
+        pn[0] += 1
+        return dict(ret=ret, tident=threading.get_ident(), pident=multiprocessing.current_process().ident, pn=pn,
+                    pln=pln.value)
