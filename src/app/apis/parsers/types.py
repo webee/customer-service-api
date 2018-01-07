10b220e9bcb355ec6a4183a1bfb07be852eb6487
wang.yi <wang.yi@qinqinxiaobao.com>
2018-01-07 23:12:30 +0800

    fix context label type.

--- src/app/apis/parsers/types.py
+++ src/app/apis/parsers/types.py
@@ -1,6 +1,5 @@
 import arrow
 from collections import namedtuple
-import json
 
 _ContextLabel = namedtuple('ContextLabel', ['path', 'uids'])
 
@@ -10,11 +9,12 @@ class ContextLabel(object):
         try:
             if not value:
                 return None
-            context_label = json.loads(value)
-            path = context_label['path']
-            uids = context_label['uids']
-            if isinstance(path, str) and isinstance(uids, list):
-                return _ContextLabel(path, uids)
+            parts = value.split(',')
+            if len(parts) != 2:
+                raise ValueError('invalid context label')
+            path = parts[0]
+            uids = parts[1].split('|')
+            return _ContextLabel(path, uids)
         except:
             pass
         raise ValueError('invalid context label')
