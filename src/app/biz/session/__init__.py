16ec81e9b43949eb61a701d0b91447b6dc893776
wang.yi <wang.yi@qinqinxiaobao.com>
2018-01-16 18:56:16 +0800

    update fetch sessions, add filter customer.

--- src/app/biz/session/__init__.py
+++ src/app/biz/session/__init__.py
@@ -10,11 +10,12 @@ order_func_map = {
 
 
 def staff_fetch_handling_sessions(app, staff, domain, type, page, per_page, context_label=None, handler=None,
+                                  customer=None,
                                   owner=None, is_online=None, unhandled_msg_count_range=None, msg_ts_range=None,
                                   tag=None, filter_self=None, sorter=None, order=None):
     q = _query_session(app, staff, domain, type, is_active=True, context_label=context_label, handler=handler,
                        owner=owner,
-                       filter_self=filter_self)
+                       filter_self=filter_self, tag=tag, is_online=is_online, customer=customer)
 
     if unhandled_msg_count_range is not None:
         start, end = unhandled_msg_count_range
@@ -29,12 +30,6 @@ def staff_fetch_handling_sessions(app, staff, domain, type, page, per_page, cont
             q = q.filter(Session.msg.has(Message.ts >= start))
         if end is not None:
             q = q.filter(Session.msg.has(Message.ts <= end))
-
-    if tag is not None:
-        q = q.filter(Session.project.has(func.array_to_string(Project.tags, '*', ',').like('%')))
-
-    if is_online is not None:
-        q = q.filter(Session.project.has(Project.is_online == is_online))
 
     order_func = order_func_map.get(order, order_func_map['descend'])
     if sorter is not None:
@@ -57,11 +52,11 @@ def staff_fetch_handling_sessions(app, staff, domain, type, page, per_page, cont
 
 
 def staff_fetch_handled_sessions(app, staff, domain, type, page, per_page, context_label=None, handler=None,
-                                 owner=None, closed_ts_range=None, is_online=None,
+                                 owner=None, closed_ts_range=None, is_online=None, customer=None,
                                  tag=None, filter_self=None, sorter=None, order=None):
     q = _query_session(app, staff, domain, type, is_active=False, context_label=context_label, handler=handler,
                        owner=owner,
-                       filter_self=filter_self)
+                       filter_self=filter_self, tag=tag, is_online=is_online, customer=customer)
 
     if closed_ts_range is not None:
         start, end = closed_ts_range
@@ -69,12 +64,6 @@ def staff_fetch_handled_sessions(app, staff, domain, type, page, per_page, conte
             q = q.filter(Session.msg.has(Message.ts >= start))
         if end is not None:
             q = q.filter(Session.msg.has(Message.ts <= end))
-
-    if tag is not None:
-        q = q.filter(Session.project.has(func.array_to_string(Project.tags, '*', ',').like('%')))
-
-    if is_online is not None:
-        q = q.filter(Session.project.has(Project.is_online == is_online))
 
     order_func = order_func_map.get(order, order_func_map['descend'])
     if sorter is not None:
@@ -92,13 +81,14 @@ def staff_fetch_handled_sessions(app, staff, domain, type, page, per_page, conte
     return res
 
 
-def _query_session(app, staff, domain, type, is_active, context_label=None, handler=None, owner=None, filter_self=None):
+def _query_session(app, staff, domain, type, is_active, context_label=None, handler=None, owner=None, filter_self=None,
+                   tag=None, is_online=None, customer=None):
     q = Session.query.join('project').options(orm.undefer('project.*')) \
         .filter(Session.is_active == is_active,
                 Session.project.has(
                     app_name=app.name,
                     domain=domain, type=type))
-    if owner is not None:
+    if owner:
         s = f'%{owner}%'
         q = q.filter(Session.project.has(
             Project.owner.has(or_(Customer.name.like(s), Customer.mobile.like(s), Customer.uid.like(s)))))
@@ -141,4 +131,17 @@ def _query_session(app, staff, domain, type, is_active, context_label=None, hand
         q = q.filter(Session.project.has(func.x_scopes_match_target(Project.scope_labels, path)))
         if len(uids) > 0 and handler is None and not only_self:
             q = q.filter(Session.handler.has(Staff.uid.in_(uids)))
+
+    if customer:
+        s = f'%{customer}%'
+        q = q.filter(Session.project.has(
+            Project.customers.any(or_(Customer.name.like(s), Customer.mobile.like(s), Customer.uid.like(s)))))
+
+    if tag:
+        s = f'%{tag}%'
+        q = q.filter(Session.project.has(func.array_to_string(Project.tags, '*', ',').like(s)))
+
+    if is_online is not None:
+        q = q.filter(Session.project.has(Project.is_online == is_online))
+
     return q
