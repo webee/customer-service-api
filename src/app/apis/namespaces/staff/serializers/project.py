6b352d48abfe39121bf43aaf65ddc560276cbab1
wang.yi <wang.yi@qinqinxiaobao.com>
2018-01-22 20:00:47 +0800

    add last_session_msg to session.

--- src/app/apis/namespaces/staff/serializers/project.py
+++ src/app/apis/namespaces/staff/serializers/project.py
@@ -19,6 +19,7 @@ session_item = api.model('Session Item', {
     'start_msg_id': fields.Integer(description='start msg id'),
     'msg_id': fields.Integer(description='last msg id'),
     'msg': fields.Nested(message, allow_null=True, description='last msg'),
+    'last_session_msg': fields.Nested(message, allow_null=True, description='last session msg'),
     'sync_msg_id': fields.Integer(description='synced msg id'),
     'handler_msg_id': fields.Integer(descripton="handler's last msg id"),
     'msg_count': fields.Integer(),
@@ -30,14 +31,15 @@ session_item = api.model('Session Item', {
 class SessionItemSchema(ma.Schema):
     class Meta:
         fields = (
-            'id', 'project', 'created', 'closed', 'handler', 'start_msg_id', 'msg_id', 'msg', 'sync_msg_id',
-            'handler_msg_id', 'msg_count', 'unsynced_count', 'unhandled_count')
+            'id', 'project', 'created', 'closed', 'handler', 'start_msg_id', 'msg_id', 'msg', 'last_session_msg',
+            'sync_msg_id', 'handler_msg_id', 'msg_count', 'unsynced_count', 'unhandled_count')
 
     project = ma.Nested(ProjectDataSchema)
     created = ma.Function(lambda s: s.created.timestamp())
     closed = ma.Function(lambda s: s.closed and s.closed.timestamp())
     handler = ma.Nested(RawStaffSchema)
     msg = ma.Nested(MessageSchema)
+    last_session_msg = ma.Nested(MessageSchema)
 
 
 session_item_schema = SessionItemSchema()
