e9ad20ed46c6d8592396c8f9cff9ecd91b17f495
wang.yi <wang.yi@qinqinxiaobao.com>
2018-01-26 19:07:25 +0800

    update biz configs: CLOSED_SESSION_ALIVE_TIME

--- src/app/config/beta.py
+++ src/app/config/beta.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 
 class App:
@@ -14,6 +15,11 @@ class App:
         'CACHE_KEY_PREFIX': NAME + '_',
         'CACHE_REDIS_URL': 'redis://127.0.0.1:6379/15'
     }
+
+
+class Biz:
+    # 已经结束的会话的复活最大时间间隔
+    CLOSED_SESSION_ALIVE_TIME = timedelta(minutes=60)
 
 
 class XChat:
